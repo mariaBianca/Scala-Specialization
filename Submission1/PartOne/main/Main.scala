0896b2e9347785a0bc862682543730fa43735921
Maria-Bianca Cindroi <bianca.cindroi@gmail.com>
2017-08-07 19:19:49 +0200

    Create Main.scala

---
+++ Submission1/PartOne/main/Main.scala
@@ -0,0 +1,11 @@
+package example
+
+object Main extends App{
+
+  //print the max of the elements in the list
+  println("max: " + Lists.max(List(1,2,3,9,5)))
+
+  //print the sum of the elements in the list
+  println("sum: " + Lists.sum(List(1,2,3,4,5,6,7,8,93)))
+
+}
